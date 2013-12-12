5dcf45af259584c0446e6576f8a0b49e02cd7ed0
Dustin Fraker <drfraker@gmail.com>
2013-12-12 15:13:25 -0700

    remove WW specific text in question and response.

--- 360-reviews/_partials/review.php
+++ 360-reviews/_partials/review.php
@@ -1,4 +1,3 @@
-
 <blockquote style="background-color: <?= $this->getBgStyle($review->{'recommendation-likelihood'}); ?>;" class="review">
     <p class="rating">
         Rated <?= $review->{'recommendation-likelihood'} . "/10 by " . $this->getInitials($review->{'customer-full-name'}); ?>     <span class="date">
@@ -9,7 +8,7 @@
         ?>
     </span>
     </p>
-    <p class="question">W|W Asked: "<?= $this->getQuestion($review->{'recommendation-likelihood'}); ?>"</p>
+    <p class="question">We Asked: "<?= $this->getQuestion($review->{'recommendation-likelihood'}); ?>"</p>
     <p class="comments">
         <?php
         if($review->comments != '')
@@ -17,7 +16,7 @@
             echo '"'.ucfirst($review->comments).'"';
         }else
         {
-            echo "No Comment Supplied.";
+            echo "No comment with rating.";
         }
 
         ?>
@@ -25,7 +24,7 @@
     <?php if($review->{'public-response'} != ''): ?>
         <blockquote class="simple" style="width: 80%; margin-left: 15px; margin-bottom: 15px; color: #4a993e;">
             <p class="response">
-                <span style="color: #666666;">Response from Wellness WORx:</span><br />
+                <span style="color: #666666;">Response from us:</span><br />
                 <?= $review->{'public-response'}; ?>
             </p>
         </blockquote><!-- end response block-->
